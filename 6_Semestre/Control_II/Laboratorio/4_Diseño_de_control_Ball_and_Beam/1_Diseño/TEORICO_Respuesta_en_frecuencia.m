ff7c4e956579f9d8a820d9a639398683b5ab9485
Joel_Zufff <diegodjzf@gmail.com>
2025-05-03 13:19:19 -0600

    mend

--- 6_Semestre/Control_II/Laboratorio/4_Diseño_de_control_Ball_and_Beam/1_Diseño/TEORICO_Respuesta_en_frecuencia.m
+++ 6_Semestre/Control_II/Laboratorio/4_Diseño_de_control_Ball_and_Beam/1_Diseño/TEORICO_Respuesta_en_frecuencia.m
@@ -4,19 +4,19 @@ k = 102;
 c = 3.1;
 %% Ganancias
 % Esclavo (Control PD)
-Kpc = 4.405;
-Kic = 0;
-Kdc = 0.1242;
+Kpe = 4.405;
+Kie = 0;
+Kde = 0.1242;
 % Maestro (Control PD)
 Kp = 8.8105;
 Ki = 0;
 Kd = 2.1899;
 %% Lazo Esclavo
 Gc = tf([k], [1 a 0]);              % Sistema sin compensar
-%Gc = tf([1 1],[1]);                 % Compensador normalizado
-Gc = tf([Kdc Kpc], 1) * Gc;     % Sistema compensado
+%Gc = tf([1 1],[1]);                % Compensador normalizado
+Gc = tf([Kde Kpe], 1) * Gc;         % Sistema compensado
 %% Lazo Maestro
-%G = feedback(Gc, -1) * tf([c], [1 0 0]);           % Sistema sin compensar
+G = tf([k*Kde*c k*Kpe*c], [1 a+k*Kde k*Kpe 0 0]);           % Sistema sin compensar
 %G = tf([1 1],[1]);                         % Compensador normalizado
 G = tf([Kd Kp], 1) * G;
 
